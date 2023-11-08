61bdf7aac7895871005d4e8896ae3945531eef27
IsmaelGabri <ismaelgabri.developer@gmail.com>
2023-11-08 10:07:10 -0300

    Creating background

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { GraphComponent } from './graph/graph.component';
+import { UserDataComponent } from './user-data/user-data.component';
+import { FilterComponent } from './filter/filter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    GraphComponent
+    GraphComponent,
+    UserDataComponent,
+    FilterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/user-data/user-data.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-data',
+  templateUrl: './user-data.component.html',
+  styleUrls: ['./user-data.component.css']
+})
+export class UserDataComponent {
+
+}
