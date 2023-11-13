7fa13e509bd07652249a8ca91d4a0abb9e251d51
IsmaelGabri <ismaelgabri.developer@gmail.com>
2023-11-13 09:56:38 -0300

    Adicionado botoes

--- src/app/user-data/user-data.component.ts
+++ src/app/user-data/user-data.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user-data',
   templateUrl: './user-data.component.html',
   styleUrls: ['./user-data.component.css']
 })
-export class UserDataComponent {
+export class UserDataComponent implements OnInit {
+
+  cursos: string[] = [""]
+
+  ngOnInit(): void {
+    
+  }
 
 }
