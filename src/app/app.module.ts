40716a394573be261c6722885f1e59a778fa645f
IsmaelGabri <ismaelgabri.developer@gmail.com>
2023-11-13 09:56:22 -0300

    Adicionadno Botoes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { GraphComponent } from './graph/graph.component';
 import { UserDataComponent } from './user-data/user-data.component';
 import { FilterComponent } from './filter/filter.component';
+import { PaginationComponent } from './pagination/pagination.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { FilterComponent } from './filter/filter.component';
     NavBarComponent,
     GraphComponent,
     UserDataComponent,
-    FilterComponent
+    FilterComponent,
+    PaginationComponent
   ],
   imports: [
     BrowserModule,
